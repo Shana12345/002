2d302b4c21ac6d338c93dfe18ec50ab78fc2b989
Shana12345 <shana_charlery@msn.com>
2021-02-03 08:19:23 +0000

    RESTful API completed-resend

--- main.go
+++ main.go
@@ -31,21 +31,57 @@ func getPets(w http.ResponseWriter, r *http.Request){
 
 //Get a pet
 func getPet(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //To get parameters
 
+	//Looping through every item to see if the item is equal to the params url
+	for _, item := range pets {
+		if item.Name ==params["name"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Pet{})
 }
 // Add a pet
 func createPet(w http.ResponseWriter, r *http.Request){
-
+	w.Header().Set("Content-Type", "application/json")
+	var Apet Pet
+	_ = json.NewDecoder(r.Body).Decode(&Apet)
+	//Apet.Name = rand.Animal //Generate a random ID -  not safe - would'nt use in production-it could generate the same name.
+	pets = append(pets, Apet)
+	json.NewEncoder(w).Encode(Apet)
 }
 
 //Update Pet
 func updatePet(w http.ResponseWriter, r *http.Request){
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range pets {
+		if item.Name == params["name"] {
+		pets = append(pets[:index], pets[index+1:]...)
+		var Apet Pet
+		_ = json.NewDecoder(r.Body).Decode(&Apet)
+		//if you was to have a random ID you want add here "pet.ID = params["id"]"
+		pets = append(pets, Apet)
+		json.NewEncoder(w).Encode(Apet)
+		return
+	}
+}
+		json.NewEncoder(w).Encode(pets)
 }
 
 //Remove a pet
 func deletePet(w http.ResponseWriter, r *http.Request){
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range pets {
+		if item.Name == params["name"] {
+		pets = append(pets[:index], pets[index+1:]...) // This is like a slice in JS - so if you were to loop through an array and slice it up
+		break
+	}
+}
+	json.NewEncoder(w).Encode(pets) //repond with pets - delete book and then give you a response of all the books
 }
 
 func main(){
@@ -53,7 +89,8 @@ func main(){
   r := mux.NewRouter()
 
 //Mock info - for DB
-pets = append(pets, Pet{Name: "Burno", Animal: "puppy", Weight: 42, Age: 1, Owner: &Owner{Firstname: "Charlie", Surname: "Bucket", OwnerPayment: "it's totally a secret"}})
+pets = append(pets, Pet{Name: "Burno", Animal: "puppy", Weight: 42, Age: 1, Owner: &Owner{Firstname: "Charlie", Surname: "Bucket", OwnerPayment: "Family"}})
+pets = append(pets, Pet{Name: "Sammy", Animal: "Dog", Weight: 50, Age: 4, Owner: &Owner{Firstname: "Jiminy", Surname: "Cricket", OwnerPayment: "Let your conscience be your guide"}})
 
 // Route Handler and EP
   r.HandleFunc("/api/pets", getPets).Methods("GET")
